0fefe07c368ba9d173b89f2d129de78d6757ecca
maoxinxin <1625183368@qq.com>
2021-01-01 17:12:52 +0800

    项目初始结构创建

---
+++ shopmail-ware/src/test/java/com/shop/shopmall/lware/ShopmailWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.shopmall.lware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopmailWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ shopmall-member/src/main/java/com/shop/shopmall/member/ShopmallMemberApplication.java
@@ -0,0 +1,13 @@
+package com.shop.shopmall.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopmallMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopmallMemberApplication.class, args);
+    }
+
+}
